8ecaaa20296f242295487388ce689e6f782ca30d
lkwoung88 <lkwoung88@gmail.com>
2024-03-21 23:00:35 +0900

    init

---
+++ springboot-nexus/src/main/java/org/example/springbootnexus/SpringbootNexusApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootnexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNexusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootNexusApplication.class, args);
+    }
+
+}
